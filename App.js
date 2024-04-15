4e52d161b1dc5ac9e4392c26c8543fa45d01bf5b
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-04-16 01:02:50 +0600

    font commit

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
+SplashScreen.preventAutoHideAsync();
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "Inter-Black": require("./assets/fonts/Outfit-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open fjdshdsjgf!</Text>
