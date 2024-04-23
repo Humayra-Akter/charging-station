37624047d0b4a86e09f29a27becea351c8455595
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-04-23 23:40:25 +0600

    navigation

---
+++ App/Navigations/TabNavigations.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigations() {
+  return <Tab.Navigator>
+    
+  </Tab.Navigator>;
+}
