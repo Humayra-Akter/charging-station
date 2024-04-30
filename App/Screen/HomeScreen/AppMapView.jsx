6390bd6267e7be817e5ba375eb3e2607d1e38393
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-04-30 23:04:27 +0600

    location car marker added

--- App/Screen/HomeScreen/AppMapView.jsx
+++ App/Screen/HomeScreen/AppMapView.jsx
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet } from "react-native";
 import React, { useContext } from "react";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { UserLocationContext } from "../../Context/UserLocationContext";
 
 export default function AppMapView() {
@@ -12,7 +12,28 @@ export default function AppMapView() {
           style={styles.map}
           provider={PROVIDER_GOOGLE}
           showsUserLocation={true}
-        />
+          region={{
+            latitude: location?.latitude,
+            longitude: location?.longitude,
+            latitudeDelta: 0.0422,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location?.latitude,
+              longitude: location?.longitude,
+            }}
+          >
+            <Image
+              source={require("./../../../assets/images/car-marker.png")}
+              style={{
+                width: 60,
+                height: 60,
+              }}
+            />
+          </Marker>
+        </MapView>
       </View>
     )
   );
