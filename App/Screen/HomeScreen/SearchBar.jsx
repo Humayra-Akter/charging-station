05abf5f9771fd3aecdd1c3e1424ed6804c2e80ef
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-05-02 22:46:40 +0600

    google api added

--- App/Screen/HomeScreen/SearchBar.jsx
+++ App/Screen/HomeScreen/SearchBar.jsx
@@ -6,12 +6,13 @@ export default function SearchBar() {
   return (
     <GooglePlacesAutocomplete
       placeholder="Search Charging Station"
+      enablePoweredByContainer={false}
       onPress={(data, details = null) => {
         // 'details' is provided when fetchDetails = true
         console.log(data, details);
       }}
       query={{
-        key: "YOUR API KEY",
+        key: "AIzaSyDelrQZavcv-2Cx0runmUCFaFuA8pV7jnU",
         language: "en",
       }}
     />
